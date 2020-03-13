f28997a3833d943d26a28dc12a9ecc9e9e741ad7
Hua-CM <njbxhzy@hotmail.com>
2020-03-13 22:57:37 +0800

    20200313

--- Code/200313.py
+++ Code/200313.py
@@ -7,6 +7,7 @@
 # @E-mail: njbxhzy@hotmail.com
 
 
+# 多数元素
 class Solution:
     def majorityElement(self, nums):
         dict_1 = {}
@@ -15,3 +16,29 @@ class Solution:
         for key, value in dict_1.items():
             if value > nums.__len__()/2:
                 return key
+
+
+# 和至少为K的最短子数组
+class Solution2:
+    def shortestSubarray(self, a, k):
+        """
+        :param a: 待处理数组
+        :param k: K
+        :return: 子数组长度
+        """
+        zhan = []
+        for x in range(1, a.__len__()+1):
+            zhan.append(sum(a[0:x]))
+
+        for y in range(zhan.__len__()):
+            y_zhan = zhan
+            y_index_list = [x for x in range(zhan.__len__())]
+            while y_zhan[y] > min(y_zhan[0:y]):
+                y_zhan.pop(0)
+                y_index_list.pop(0)
+            while y_zhan[0] + k > y_zhan[index]:
+                y_zhan.pop()
+
+
+
+
--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 |:----:|:----:|:-----|:---------|---------:|---------:|
 |1|简单|[字符串的最大公因子](https://leetcode-cn.com/problems/greatest-common-divisor-of-strings/)|[200312.py](Code/200312.py)|36ms|13.3MB|
 |1|简单|[多数元素](https://leetcode-cn.com/problems/majority-element/)|[200313.py](Code/200313.py)|48ms|15.2MB|
+|1|困难|[和至少为 K 的最短子数组](https://leetcode-cn.com/problems/shortest-subarray-with-sum-at-least-k/)|[200313.py](Code/200313.py)|没做出来|留着填坑|
